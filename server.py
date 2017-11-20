1eb5ac4a8b8ad8856f98737671696bfc0464d0f0
Sergio  De La Fuente Garcia <sfuente@gamma14.aulas.gsyc.urjc.es>
2017-11-20 16:56:02 +0100

    server1.1

--- server.py
+++ server.py
@@ -11,17 +11,24 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
+    def check_method(self, method):
+        methods = ['INVITE', 'ACK', 'BYE']
+        if method in methods:
+            pass
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            data = self.rfile.read().decode('utf-8')
+            print(data.split(' '))
+            #metodo,sip_address,protocol = data.split(' ')
+            #self.check_method(metodo)
+            print("El cliente nos manda " + data)
 
             # Si no hay más líneas salimos del bucle infinito
-            if not line:
+            if not data:
                 break
 
 if __name__ == "__main__":
