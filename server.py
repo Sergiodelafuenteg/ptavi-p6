4717f8a2a425cef73082973bad7cd480aff9d5c3
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-11-26 18:54:27 +0100

    server1.2

--- server.py
+++ server.py
@@ -14,35 +14,30 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     def check_method(self, method):
         methods = ['INVITE', 'ACK', 'BYE']
         if method in methods:
-            if method == INVITE:
-                self.data_send = "SIP 2.0 100 Trying\r\n\r\n" +
+            if method == 'INVITE':
+                print("gooooo")
+                self.data_send = ("SIP 2.0 100 Trying\r\n\r\n" +
                                 "SIP 2.0 180 Ringing\r\n\r\n" +
-                                "SIP 2.0 200 OK\r\n\r\n"
-            elif method == BYE:
+                                "SIP 2.0 200 OK\r\n\r\n")
+            elif method == 'BYE':
                 self.data_send = "SIP 2.0 200 OK\r\n\r\n"
-            elif method == ACK:
+            elif method == 'ACK':
                 self.data_send = ""
-        else
+        else:
             data_send = "SIP 2.0 400 Bad request\r\n\r\n"
-        self.wfile.write(b(data_send, 'utf-8'))
+            self.wfile.write(bytes(data_send, 'utf-8'))
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            data = self.rfile.read().decode('utf-8')
-            print(data.split(' '))
-            #metodo,sip_address,protocol = data.split(' ')
-            #self.check_method(metodo)
-            print("El cliente nos manda " + data)
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not data:
-                break
+        # Leyendo línea a línea lo que nos envía el cliente
+        data = self.rfile.read().decode('utf-8')
+        print(data.split(' '))
+        metodo,sip_address,protocol = data.split(' ')
+        self.check_method(metodo)
+        print("El cliente nos manda " + metodo)
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
+    print("Listening...")
     serv.serve_forever()
