b40dfeb5a095efe2a5ddeaad57589264da5e409b
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-11-26 18:54:13 +0100

    client1.2

--- client.py
+++ client.py
@@ -30,14 +30,11 @@ LINE = '¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
-
-    print("Enviando: " + LINE)
-    my_socket.send(bytes(DATA, 'utf-8') + b'\r\n')
+    print("Enviando:", DATA)
+    my_socket.send(bytes(DATA, 'utf-8'))
     data = my_socket.recv(1024)
-
+    print(data.decode('utf-8'))
     print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
 
 print("Fin.")
