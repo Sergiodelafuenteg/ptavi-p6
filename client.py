3f4a6dbfaa4352c1b29608bd3d1cdf98e08a83ba
Sergio  De La Fuente Garcia <sfuente@gamma14.aulas.gsyc.urjc.es>
2017-11-27 16:58:35 +0100

    client sin terminar

--- client.py
+++ client.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""
-Programa cliente que abre un socket a un servidor
-"""
+"""Programa cliente que abre un socket a un servidor."""
 
 import socket
 import sys
-# Cliente UDP simple.
-
-# Dirección IP del servidor.
 
 try:
     _, METODO, SIP_ADDRESS = sys.argv
@@ -21,11 +16,9 @@ login,SERVER = SERVER.split('@')
 print(SERVER,PORT,login)
 PORT = int(PORT)
 METODO = METODO.upper()
-PROTOCOL = 'SIP/2.0\r\n'
+PROTOCOL = 'SIP/2.0\r\n\r\n'
 DATA = ' '.join([METODO.upper(), "sip:" + SIP_ADDRESS, PROTOCOL])
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
@@ -34,7 +27,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.send(bytes(DATA, 'utf-8'))
     data = my_socket.recv(1024)
     cod_answer = data.decode('utf-8').split(' ')[-2]
-    if cod_answer == '200':
+    if (cod_answer == '200') and (METODO != 'BYE'):
         DATA = ' '.join(["ACK", "sip:" + SIP_ADDRESS, PROTOCOL])
         my_socket.send(bytes(DATA, 'utf-8'))
         print(cod_answer)
