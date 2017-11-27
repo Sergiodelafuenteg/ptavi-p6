0b77c92f6313282a917d0c07ce3d6b2c23e27878
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-11-27 20:21:49 +0100

    style-pep8

--- server.py
+++ server.py
@@ -18,8 +18,8 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             if (protocol == 'SIP/2.0\r\n\r\n') and (sip[0:4] == 'sip:'):
                 if method == 'INVITE':
                     data_send = ("SIP/2.0 100 Trying\r\n\r\n" +
-                                "SIP/2.0 180 Ringing\r\n\r\n" +
-                                "SIP/2.0 200 OK\r\n\r\n")
+                                 "SIP/2.0 180 Ringing\r\n\r\n" +
+                                 "SIP/2.0 200 OK\r\n\r\n")
                 elif method == 'BYE':
                     data_send = "SIP/2.0 200 OK\r\n\r\n"
                 elif method == 'ACK':
