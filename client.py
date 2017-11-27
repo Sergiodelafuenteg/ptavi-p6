6f2bd563ba9d8abf699adda7565dc7d42dc7bd53
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-11-27 20:13:12 +0100

    clientfinished

--- client.py
+++ client.py
@@ -9,28 +9,20 @@ try:
     _, METODO, SIP_ADDRESS = sys.argv
 
 except IndexError:
-    sys.exit('Usage: client.py ip metodo sip_address')
+    sys.exit('Usage: python3 client.py method receiver@IP:SIPport')
 
 SERVER,PORT = SIP_ADDRESS.split(':')
 login,SERVER = SERVER.split('@')
-print(SERVER,PORT,login)
 PORT = int(PORT)
 METODO = METODO.upper()
 PROTOCOL = 'SIP/2.0\r\n\r\n'
 DATA = ' '.join([METODO.upper(), "sip:" + SIP_ADDRESS, PROTOCOL])
 
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
-    print("Enviando:", DATA)
     my_socket.send(bytes(DATA, 'utf-8'))
     data = my_socket.recv(1024)
     cod_answer = data.decode('utf-8').split(' ')[-2]
     if (cod_answer == '200') and (METODO != 'BYE'):
         DATA = ' '.join(["ACK", "sip:" + SIP_ADDRESS, PROTOCOL])
         my_socket.send(bytes(DATA, 'utf-8'))
-        print(cod_answer)
-    print('Recibido -- ', data.decode('utf-8'))
-
-print("Fin.")
